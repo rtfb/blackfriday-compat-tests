a6c19e0677de14275434441f61fad86faa1133ad
Vytautas Šaltenis <vytas@rtfb.lt>
2015-04-28 21:22:12 +0300

    A package to test blackfriday's compat layer
    Currently just a copy of a function that I use elsewhere. Should grow a
    bit to use all the bits and pieces of public API. The point here is to
    have it build on Travis and watch for regressions while we improve the
    API towards v2.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Blackfriday compat test.
+
+[![Build status][travis-image]][travis-url]
+
+## What is this?
+
+This is a little package to exercise an interface compatibility layer of
+[blackfriday](https://github.com/russross/blackfriday/) between versions
+1.x and 2.0.
+
+[travis-image]: https://travis-ci.org/rtfb/blackfriday-compat-tests.svg?branch=master
+[travis-url]: https://travis-ci.org/rtfb/blackfriday-compat-tests
---
+++ testcompat.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	//"github.com/russross/blackfriday"
+	blackfriday "gopkg.in/rtfb/blackfriday.v2/compat"
+)
+
+func main() {
+	md := "**foo**"
+	htmlFlags := 0
+	htmlFlags |= blackfriday.HTML_USE_XHTML
+	htmlFlags |= blackfriday.HTML_USE_SMARTYPANTS
+	htmlFlags |= blackfriday.HTML_SMARTYPANTS_FRACTIONS
+	htmlFlags |= blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
+	renderer := blackfriday.HtmlRenderer(htmlFlags, "", "")
+	extensions := 0
+	extensions |= blackfriday.EXTENSION_NO_INTRA_EMPHASIS
+	extensions |= blackfriday.EXTENSION_TABLES
+	extensions |= blackfriday.EXTENSION_FENCED_CODE
+	extensions |= blackfriday.EXTENSION_AUTOLINK
+	extensions |= blackfriday.EXTENSION_STRIKETHROUGH
+	extensions |= blackfriday.EXTENSION_SPACE_HEADERS
+	extensions |= blackfriday.EXTENSION_HEADER_IDS
+	println(blackfriday.Markdown([]byte(md), renderer, extensions))
+}
